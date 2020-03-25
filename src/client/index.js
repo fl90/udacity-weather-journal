8e58086a77386517c583227906e0af53dce35b82
FlorianLiesenfeld <florian.liesenfeld@247grad.de>
2020-03-25 18:18:02 +0100

    Review comments

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,9 @@
 import { getGeoData } from './js/geonames';
 import { calcTripLength } from './js/geonames';
-import { testGeoData } from './js/geonames';
 
 import './styles/style.scss'
 
 export {
     getGeoData,
     calcTripLength,
-    testGeoData
 }
--- src/client/js/geonames.js
+++ src/client/js/geonames.js
@@ -7,55 +7,6 @@ function getGeoData(event){
     let locationName = document.getElementById("zip");
     let startDate = document.getElementById("start-date");
     let endDate = document.getElementById("end-date");
-    
-    let apiUrl = "http://api.geonames.org/postalCodeSearchJSON?";
-
-    retrieveData(apiUrl + "placename=" + locationName.value + "&username=szensemann")
-    .then(function(response, error){
-        let lat = response.postalCodes[0].lat;
-        let lng = response.postalCodes[0].lng;
-        let zip = response.postalCodes[0].postalCode;
-
-        let start = new Date(startDate.value);
-        let end = new Date(endDate.value);
-
-        const diffDays = Math.ceil(Math.abs((start - new Date()) / oneDay));
-        const tripLength = calcTripLength(start, end);// Math.ceil(Math.abs((end - start) / oneDay));
-        document.getElementById("trip-length").innerHTML = tripLength + " day(s)";
-
-        postData('http://localhost:3003/darksky/forecast',{
-            lat: lat,
-            lng: lng,
-            start: (start.getTime() / 1000)
-        }).then(function(resp){
-            document.getElementById("trip-weather").innerHTML = resp.daily.data[0].summary;
-            document.getElementById("temp-max").innerHTML = resp.daily.data[0].temperatureHigh + " C°";
-            document.getElementById("temp-low").innerHTML = resp.daily.data[0].temperatureLow + " C°";
-        })
-
-        postData('http://localhost:3003/pixabay/image', {
-            destination: locationName.value
-        }).then(function(responso){
-
-            // show image in html
-            var imageUrl = responso.hits[0].webformatURL;
-            document.getElementById("trip-image").setAttribute("src", imageUrl);
-        })
-
-        // fill in the html
-        document.getElementById("destination").innerHTML = locationName.value + ", " + response.postalCodes[0].countryCode + " is " + diffDays + " day(s) away";
-        document.getElementById("trip-start-date").innerHTML = start.getDate() + "." + (start.getMonth()+1) + "." + start.getFullYear();
-        document.getElementById("trip-end-date").innerHTML = end.getDate() + "." + (end.getMonth()+1) + "." + end.getFullYear();
-    });
-    
-};
-
-function testGeoData(event){
-    event.preventDefault();
-
-    let locationName = document.getElementById("zip");
-    let startDate = document.getElementById("start-date");
-    let endDate = document.getElementById("end-date");
     let start = new Date(startDate.value);
     let end = new Date(endDate.value);
 
@@ -147,4 +98,4 @@ const postData = async ( url = '', data = {})=>{
     }
 }
 
-export { getGeoData, retrieveData, calcTripLength, testGeoData}
+export { getGeoData, retrieveData, calcTripLength}
